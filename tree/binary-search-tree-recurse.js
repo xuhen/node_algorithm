2e9f6208fb789d8293ea3c07c36f840363d7be1b
Edward xu <xuhengberkeley@163.com>
2020-05-06 15:35:16 +0800

    feat: add binary search tree

---
+++ tree/binary-search-tree-recurse.js
@@ -0,0 +1,133 @@
+// https://www.geeksforgeeks.org/implementation-binary-search-tree-javascript/
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    // function to be implemented 
+    // insert(data) 
+    // remove(data) 
+
+
+    // Helper function 
+    // findMinNode() 
+    // getRootNode() 
+    // inorder(node) 
+    // preorder(node)                
+    // postorder(node) 
+    // search(node, data) 
+
+    insert(data) {
+        var newNode = new Node(data);
+
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode)
+            }
+        }
+    }
+
+    remove(data) {
+        this.root = this.removeNode(this.root, data);
+    }
+
+    removeNode(node, key) {
+        if (node === null) {
+            return null;
+        } else if (key < node.data) {
+            node.left = this.removeNode(node.left, key);
+            return node;
+        } else if (key > node.data) {
+            node.right = this.removeNode(node.right, key);
+            return node;
+        } else {
+            if (node.left === null && node.right === null) {
+                node = null;
+                return node;
+            } else if (node.left === null) {
+                node = node.right;
+                return node;
+            } else if (node.right === null) {
+                node = node.left;
+                return node;
+            } else {
+                var aux = this.findMinNode(node.right);
+                node.data = aux.data;
+
+                node.right = this.removeNode(node.right, aux.data);
+                return node;
+            }
+        }
+
+    }
+    findMinNode(node) {
+        if (node.left === null) {
+            return node;
+        } else {
+            return this.findMinNode(node.left)
+        }
+    }
+    getRootNode() {
+        return this.root;
+    }
+    search(node, data) {
+        if (node === null) {
+            return null;
+        } else if (node.data > data) {
+            return this.search(node.left, data);
+        } else if (node.data < data) {
+            return this.search(node.right, data);
+        } else {
+            return node;
+        }
+    }
+}
+
+
+var bst = new BinarySearchTree();
+
+bst.insert(15)
+bst.insert(25)
+bst.insert(10)
+bst.insert(7)
+bst.insert(22)
+bst.insert(17)
+bst.insert(13)
+bst.insert(5)
+bst.insert(9)
+bst.insert(27)
+bst.insert(30)
+
+bst.remove(22);
+
+var root = bst.getRootNode();
+var r = bst.search(root, 22);
+
+
+console.log(bst, r);
